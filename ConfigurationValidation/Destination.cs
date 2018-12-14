487d33dbcb9e18fe40dc1833e2f6a0327e3e953d
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-14 22:13:16 +0100

    Configuration validation

---
+++ ConfigurationValidation/Destination.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationValidation
+{
+    public class Destination
+    {
+        public int SomeValuefff { get; set; }
+    }
+}
---
+++ ConfigurationValidation/Program.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mapper.Initialize(cfg =>
+            //    cfg.CreateMap<Source, Destination>());
+
+            //try
+            //{
+            //    Mapper.Configuration.AssertConfigurationIsValid();
+            //}            
+            //catch(AutoMapperConfigurationException)
+            //{
+            //    Console.WriteLine("Invalid configuration");
+            //}
+
+            Mapper.Initialize(cfg =>
+              cfg.CreateMap<Source, Destination>()
+                .ForMember(dest => dest.SomeValuefff, opt => opt.Ignore())
+            );
+
+            Mapper.Configuration.AssertConfigurationIsValid();
+
+        }
+    }
+}
