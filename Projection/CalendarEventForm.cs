02472f61b6b63b0bde153e0c044098eb97cc2db1
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-14 22:08:06 +0100

    Projection

---
+++ Projection/CalendarEventForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projection
+{
+    public class CalendarEventForm
+    {
+        public DateTime EventDate { get; set; }
+        public int EventHour { get; set; }
+        public int EventMinute { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Projection/Program.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Model
+            var calendarEvent = new CalendarEvent
+            {
+                Date = new DateTime(2008, 12, 15, 20, 30, 0),
+                Title = "Company Holiday Party"
+            };
+
+            // Configure AutoMapper
+            Mapper.Initialize(cfg =>
+              cfg.CreateMap<CalendarEvent, CalendarEventForm>()
+                .ForMember(dest => dest.EventDate, opt => opt.MapFrom(src => src.Date.Date))
+                .ForMember(dest => dest.EventHour, opt => opt.MapFrom(src => src.Date.Hour))
+                .ForMember(dest => dest.EventMinute, opt => opt.MapFrom(src => src.Date.Minute)));
+
+            // Perform mapping
+            CalendarEventForm form = Mapper.Map<CalendarEvent, CalendarEventForm>(calendarEvent);
+
+            //form.EventDate.ShouldEqual(new DateTime(2008, 12, 15));
+            //form.EventHour.ShouldEqual(20);
+            //form.EventMinute.ShouldEqual(30);
+            //form.Title.ShouldEqual("Company Holiday Party");
+        }
+    }
+}
