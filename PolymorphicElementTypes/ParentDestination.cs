0aa49833ae68ae257096241996680b1a8209884a
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-14 22:26:12 +0100

    Polymorphic element types in collections

---
+++ PolymorphicElementTypes/ParentDestination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphicElementTypes
+{
+    public class ParentDestination
+    {
+        public int Value1 { get; set; }
+    }
+
+}
---
+++ PolymorphicElementTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphicElementTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Initialize(c => {
+                c.CreateMap<ParentSource, ParentDestination>()
+                     .Include<ChildSource, ChildDestination>();
+                c.CreateMap<ChildSource, ChildDestination>();
+            });
+
+            var sources = new[]
+                {
+                    new ParentSource(),
+                    new ChildSource(),
+                    new ParentSource()
+                };
+
+            var destinations = Mapper.Map<ParentSource[], ParentDestination[]>(sources);
+
+            //destinations[0].ShouldBeInstanceOf<ParentDestination>();
+            //destinations[1].ShouldBeInstanceOf<ChildDestination>();
+            //destinations[2].ShouldBeInstanceOf<ParentDestination>();
+        }
+    }
+}
