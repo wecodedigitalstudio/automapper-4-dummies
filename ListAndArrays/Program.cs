f7059804926145ae8da9d75d47a1c53e8bb6ba69
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-14 22:21:40 +0100

    List and arrays

---
+++ ListAndArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAndArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<Source, Destination>());
+
+            var sources = new[]
+                {
+                    new Source { Value = 5 },
+                    new Source { Value = 6 },
+                    new Source { Value = 7 }
+                };
+
+            IEnumerable<Destination> ienumerableDest = Mapper.Map<Source[], IEnumerable<Destination>>(sources);
+            ICollection<Destination> icollectionDest = Mapper.Map<Source[], ICollection<Destination>>(sources);
+            IList<Destination> ilistDest = Mapper.Map<Source[], IList<Destination>>(sources);
+            List<Destination> listDest = Mapper.Map<Source[], List<Destination>>(sources);
+            Destination[] arrayDest = Mapper.Map<Source[], Destination[]>(sources);
+        }
+    }
+}
