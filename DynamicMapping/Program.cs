3f94f869eb86a24e480855f72f808c2011fdc898
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-15 10:08:42 +0100

    Dynamic mapping

---
+++ DynamicMapping/Program.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicMapping
+{
+
+    public class Foo
+    {
+        public int Bar { get; set; }
+        public int Baz { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic foo = new { Bar = 5, Baz = 6 };
+
+            Mapper.Initialize(cfg => { });
+
+            var result = Mapper.Map<Foo>(foo);
+            //result.Bar.ShouldEqual(5);
+            //result.Baz.ShouldEqual(6);
+
+
+        }
+    }
+}
