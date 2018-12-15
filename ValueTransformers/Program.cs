c0a1559f3329d2d3b2a45662bc2e1bb74cccb415
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-15 09:43:06 +0100

    Value Transformers

---
+++ ValueTransformers/Program.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTransformers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => {
+                cfg.ValueTransformers.Add<string>(val => val + "!!!");
+            });
+
+            var source = new Source { Value = "Hello" };
+            var dest = Mapper.Map<Dest>(source);
+
+            //dest.Value.ShouldBe("Hello!!!");
+        }
+    }
+}
