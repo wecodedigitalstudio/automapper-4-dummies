b52e8ef1f299625b8b32c62d02703f6ed0eebf38
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-15 09:55:10 +0100

    Before and After Map Action

---
+++ BeforeAfterMap/Program.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeforeAfterMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => {
+                cfg.CreateMap<Source, Dest>()
+                  .BeforeMap((src, dest) => src.Value = src.Value + 10)
+                  .AfterMap((src, dest) => dest.Name = "John");
+            });
+
+            //int i = 10;
+            //Mapper.Map<Source, Dest>(src, opt => {
+            //    opt.BeforeMap((src, dest) => src.Value = src.Value + i);
+            //    opt.AfterMap((src, dest) => dest.Name = HttpContext.Current.Identity.Name);
+            //});
+
+        }
+    }
+}
