1d75505f684084216dbd7f269e298172407f1bd3
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-15 10:02:29 +0100

    Queryable Extensions

---
+++ QueryableExtensions/OrderLineDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryableExtensions
+{
+    public class OrderLineDTO
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public string Item { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
---
+++ QueryableExtensions/Program.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryableExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg =>
+                cfg.CreateMap<OrderLine, OrderLineDTO>()
+                .ForMember(dto => dto.Item, conf => conf.MapFrom(ol => ol.Item.Name)));
+
+
+        }
+
+        //public static List<OrderLineDTO> GetLinesForOrder(int orderId)
+        //{
+        //    using (var context = new orderEntities())
+        //    {
+        //        return context.OrderLines.Where(ol => ol.OrderId == orderId)
+        //                 .ProjectTo<OrderLineDTO>().ToList();
+        //    }
+        //}
+    }
+}
