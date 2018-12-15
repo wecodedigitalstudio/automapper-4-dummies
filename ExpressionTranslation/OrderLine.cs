a89e453a82c71e8981f3403e00bef60cec8fa3fc
Roberto Borsini <roberto.borsini@gmail.com>
2018-12-15 10:13:06 +0100

    Expression Translation

---
+++ ExpressionTranslation/OrderLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTranslation
+{
+    public class OrderLine
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public Item Item { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
---
+++ ExpressionTranslation/Program.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTranslation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<OrderLine, OrderLineDTO>()
+                  .ForMember(dto => dto.Item, conf => conf.MapFrom(ol => ol.Item.Name));
+                cfg.CreateMap<OrderLineDTO, OrderLine>()
+                  .ForMember(ol => ol.Item, conf => conf.MapFrom(dto => dto));
+                cfg.CreateMap<OrderLineDTO, Item>()
+                  .ForMember(i => i.Name, conf => conf.MapFrom(dto => dto.Item));
+            });
+
+            Expression<Func<OrderLineDTO, bool>> dtoExpression = dto => dto.Item.StartsWith("A");
+            var expression = Mapper.Map<Expression<Func<OrderLine, bool>>>(dtoExpression);
+
+        }
+    }
+}
